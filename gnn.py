3a12d0fc685ae8bc3c48b8ae5e60eb2f309a4d2b
DentonJC <DentonJC@users.noreply.github.com>
2021-05-10 15:22:32 +0200

    Create gnn.py

---
+++ gnn.py
@@ -0,0 +1,147 @@
+import numpy as np
+import dgl
+import torch
+import dgl.nn as dglnn
+import torch.nn as nn
+import torch.nn.functional as F
+import dgl.function as fn
+import networkx as nx
+
+
+class SAGE(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats):
+        super().__init__()
+        self.conv1 = dglnn.SAGEConv(
+            in_feats=in_feats, out_feats=hid_feats, aggregator_type='mean')
+        self.conv2 = dglnn.SAGEConv(
+            in_feats=hid_feats, out_feats=out_feats, aggregator_type='mean')
+
+    def forward(self, graph, inputs):
+        # inputs are features of nodes
+        h = self.conv1(graph, inputs)
+        h = F.relu(h)
+        h = self.conv2(graph, h)
+        return h
+
+
+class DotProductPredictor(nn.Module):
+    def forward(self, graph, h):
+        # h contains the node representations computed from the GNN defined
+        # in the node classification section (Section 5.1).
+        with graph.local_scope():
+            graph.ndata['h'] = h
+            graph.apply_edges(fn.u_dot_v('h', 'h', 'score'))
+            return graph.edata['score']
+
+
+class MLPPredictor(nn.Module):
+    def __init__(self, in_features, out_classes):
+        super().__init__()
+        self.W = nn.Linear(in_features * 2, out_classes)
+
+    def apply_edges(self, edges):
+        h_u = edges.src['h']
+        h_v = edges.dst['h']
+        score = self.W(torch.cat([h_u, h_v], 1))
+        return {'score': score}
+
+    def forward(self, graph, h):
+        # h contains the node representations computed from the GNN defined
+        # in the node classification section (Section 5.1).
+        with graph.local_scope():
+            graph.ndata['h'] = h
+            graph.apply_edges(self.apply_edges)
+            return graph.edata['score']
+
+class Model(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features):
+        super().__init__()
+        self.sage = SAGE(in_features, hidden_features, out_features)
+        self.pred = DotProductPredictor()
+    def forward(self, g, x):
+        h = self.sage(g, x)
+        return self.pred(g, h)
+
+
+def build_graph():
+    # All 78 edges are stored in two numpy arrays. One for source endpoints
+    # while the other for destination endpoints.
+    src = np.array([1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 10, 10,
+        10, 11, 12, 12, 13, 13, 13, 13, 16, 16, 17, 17, 19, 19, 21, 21,
+        25, 25, 27, 27, 27, 28, 29, 29, 30, 30, 31, 31, 31, 31, 32, 32,
+        32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 33, 33, 33, 33, 33, 33,
+        33, 33, 33, 33, 33, 33, 33, 33, 33, 33])
+    dst = np.array([0, 0, 1, 0, 1, 2, 0, 0, 0, 4, 5, 0, 1, 2, 3, 0, 2, 2, 0, 4,
+        5, 0, 0, 3, 0, 1, 2, 3, 5, 6, 0, 1, 0, 1, 0, 1, 23, 24, 2, 23,
+        24, 2, 23, 26, 1, 8, 0, 24, 25, 28, 2, 8, 14, 15, 18, 20, 22, 23,
+        29, 30, 31, 8, 9, 13, 14, 15, 18, 19, 20, 22, 23, 26, 27, 28, 29, 30,
+        31, 32])
+    # Edges are directional in DGL; Make them bi-directional.
+    u = np.concatenate([src, dst])
+    v = np.concatenate([dst, src])
+    # Construct a DGLGraph
+    return dgl.DGLGraph((u, v))
+    
+
+G = build_graph()
+nx_G = G.to_networkx().to_undirected()
+
+# synthetic node and edge features, as well as edge labels
+G.ndata['feature'] = torch.randn(34, 10)
+G.edata['feature'] = torch.randn(156, 10)
+G.edata['label'] = torch.randn(156)
+
+
+# synthetic train-validation-test splits
+G.edata['train_mask'] = torch.zeros(156, dtype=torch.bool).bernoulli(0.6)
+
+node_features = G.ndata['feature']
+edge_label = G.edata['label']
+train_mask = G.edata['train_mask']
+
+model = Model(10, 20, 5)
+opt = torch.optim.Adam(model.parameters())
+all_logits = []
+for epoch in range(50):
+    pred = model(G, node_features)
+    all_logits.append(pred.detach())
+    loss = ((pred[train_mask] - edge_label[train_mask]) ** 2).mean()
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+    print('Epoch %d | Loss: %.4f' % (epoch, loss.item()))
+  
+"""      
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+#pos = nx.kamada_kawai_layout(nx_G)
+#nx.draw(nx_G, pos, with_labels=True, node_color=[[.7, .7, .7]])
+#plt.show()
+
+def draw(i):
+    cls1color = '#00FFFF'
+    cls2color = '#FF00FF'
+    pos = {}
+    colors = []
+    for v in range(34):
+        pos[v] = all_logits[i][v].numpy()
+        cls = pos[v].argmax()
+        colors.append(cls1color if cls else cls2color)
+    ax.cla()
+    ax.axis('off')
+    ax.set_title('Epoch: %d' % i)
+    nx.draw_networkx(nx_G.to_undirected(), nx.kamada_kawai_layout(nx_G), node_color=colors, with_labels=True, node_size=300, ax=ax)
+
+fig = plt.figure(dpi=150)
+fig.clf()
+ax = fig.subplots()
+draw(49)  # draw the prediction of the first epoch
+plt.show()
+plt.close()
+"""
+#ani = animation.FuncAnimation(fig, draw, frames=len(all_logits), interval=200)
+#plt.show()
+#FFwriter = animation.FFMpegWriter()
+#lin_ani.save('animation.mp4', writer = FFwriter, fps=10)
+
