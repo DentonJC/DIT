fe06a0d6eb93ef864ac67905090ab8eb9b6553be
DentonJC <DentonJC@users.noreply.github.com>
2021-06-04 12:16:33 +0200

    gnn fix

--- gnn.py
+++ gnn.py
@@ -72,7 +72,7 @@ class Model(nn.Module):
 def build_graph():
     # All 78 edges are stored in two numpy arrays. One for source endpoints
     # while the other for destination endpoints.
-     src = np.array([1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 10, 10,
+    src = np.array([1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 10, 10,
         10, 11, 12, 12, 13, 13, 13, 13, 16, 16, 17, 17, 19, 19, 21, 21,
         25, 25, 27, 27, 27, 28, 29, 29, 30, 30, 31, 31, 31, 31, 32, 32,
         32, 32, 32, 32, 32, 32, 32, 32, 32, 33, 33, 33, 33, 33, 33, 33,
